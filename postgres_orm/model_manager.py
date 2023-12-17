b79f53c415a37c58acf0fb3b2eaf1b0ab1040625
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-12-17 13:48:34 +0400

    implement metaclass that turns table class varibles into iterable fields

---
+++ postgres_orm/model_manager.py
@@ -0,0 +1,12 @@
+class TableMeta(type):
+    """this is a metaclass that will turn class variables to fields
+    so tht I can iterate over them and use them in a query"""
+    def __new__(cls, name, bases, dct):
+        fields = {k: v for k, v in dct.items() if not k.startswith("__")}
+        dct['fields'] = fields
+
+        return super().__new__(cls, name, bases, dct)
+
+
+class BaseModel(metaclass=TableMeta):
+    table_name = None

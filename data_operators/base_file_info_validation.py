e706f847d16dd7d17dab86f4b011773c953c1458
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-12-18 22:10:24 +0400

    add commits for pydantic validation class

--- data_operators/base_file_info_validation.py
+++ data_operators/base_file_info_validation.py
@@ -2,6 +2,11 @@ from pydantic import BaseModel, Field, field_validator
 from typing import Literal, Dict
 from datetime import datetime
 import re
+from utils.orm_logger import setup_logging
+import logging
+
+setup_logging()
+logger = logging.getLogger(__name__)
 
 
 class BaseFileInfo(BaseModel):
@@ -9,7 +14,7 @@ class BaseFileInfo(BaseModel):
     file_extension: Literal["csv", "json"]
     file_size: float = 0
     date_created: datetime = Field(default_factory=lambda: datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-    headers_and_types: Dict[str, str] = {} # if file is not empty, will contain column/types, key/types
+    headers_and_types: Dict[str, str] = {}  # if file is not empty, will contain column/types, key/types
 
     @field_validator("file_name")
     def validate_name(cls, value: str):
@@ -17,6 +22,7 @@ class BaseFileInfo(BaseModel):
         tes&t_fil*e -> test_file
         """
         if not value:
+            logger.error(f"file name name supported, new name: {cls._clean_file_name(value)}")
             raise ValueError("name not valid")
         return cls._clean_file_name(value)
 
@@ -26,6 +32,7 @@ class BaseFileInfo(BaseModel):
 
         cant be forced to validate"""
         if not value or value not in ["csv", "json"]:
+            logger.error(f"file should either be csv or json")
             raise ValueError("not valid file extension")
         return value
 

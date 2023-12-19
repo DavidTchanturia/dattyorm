cdbdf65fde26a4bf6219c106169bec9311abd26b
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-12-19 11:06:07 +0400

    change pripary key type to BIGSERIAL

--- postgres_orm/columns.py
+++ postgres_orm/columns.py
@@ -10,7 +10,7 @@ class Column:
         constraints = ''
         if self.nullable:
             if self.default is not None:
-                constraints += f'DEFAULT {self.default}'
+                constraints += f'DEFAULT {str(self.default)}'
         else:
             constraints += 'NOT NULL'
         if self.unique:
@@ -21,7 +21,7 @@ class Column:
 class PrimaryKey(Column):
     def __init__(self, auto_increment: bool = True) -> None:
         super().__init__(unique=True, nullable=False, default=None)
-        self.type = 'SERIAL'
+        self.type = 'BIGSERIAL' if auto_increment else 'INTEGER'  # BIGSERIAL avoids creating seperate sequence for autoincremented id
         self.auto_increment = auto_increment
 
     def __repr__(self):
@@ -31,7 +31,7 @@ class PrimaryKey(Column):
 
 class Integer(Column):
     def __init__(self, unique: bool = False,
-                 nullable: bool = True, default: str = None) -> None:
+                 nullable: bool = True, default: int = None) -> None:
         super().__init__(unique, nullable, default)
         self.type = 'INTEGER'
 
@@ -44,22 +44,22 @@ class Text(Column):
 
 
 class VarChar(Column):
-    def __init__(self, unique: bool = False,
+    def __init__(self, size: int = 255, unique: bool = False,
                  nullable: bool = True, default: str = None) -> None:
         super().__init__(unique, nullable, default)
-        self.type = 'VARCHAR(255)'
+        self.type = f'VARCHAR({size})'  # either create VARCHAR(255) or any other size as your wishes
 
 
 class Double(Column):
     def __init__(self, unique: bool = False,
-                 nullable: bool = True, default: str = None) -> None:
+                 nullable: bool = True, default: float = None) -> None:
         super().__init__(unique, nullable, default)
         self.type = 'DOUBLE'
 
 
 class Float(Column):
     def __init__(self, unique: bool = False,
-                 nullable: bool = True, default: str = None) -> None:
+                 nullable: bool = True, default: float = None) -> None:
         super().__init__(unique, nullable, default)
         self.type = 'FLOAT'
 
@@ -83,3 +83,4 @@ class DateTime(Column):
                  nullable: bool = True, default: str = None) -> None:
         super().__init__(unique, nullable, default)
         self.type = 'DATETIME'
+
